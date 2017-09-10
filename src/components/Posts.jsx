131b2a21507ce3a2a95aaf3f9ac5e327cd50dbb6
abumalick <abdullah.hilson@gmail.com>
2017-09-10 13:17:43 +0200

    feat: 404 pages on non existant categories

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import Button from 'antd/lib/button'
+import Spin from 'antd/lib/spin'
 import Table from 'antd/lib/table'
 import VoteScore from './VoteScore'
 import {
@@ -44,7 +45,7 @@ class Posts extends Component {
     this.props.dispatch(deletePost(id))
   }
   render() {
-    const {dispatch, posts} = this.props
+    const {category: categoryPath, categoryName, dispatch, posts} = this.props
     const columns = [
       {
         title: 'Score',
@@ -120,50 +121,84 @@ class Posts extends Component {
         ),
       }),
     )
-    return (
-      <div>
-        {posts.status === 'fetched' &&
-          (posts.data.length ? (
-            <Table columns={columns} dataSource={data} pagination={false} />
-          ) : (
-            <p>There is currently no posts in this category.</p>
-          ))}
+    if (posts.status === 'fetched')
+      return (
+        <div>
+          {!categoryPath || categoryName ? (
+            <div>
+              <h1 className="pl3 pb3 ma0 f3">
+                {categoryName ? (
+                  `Posts for category ${categoryName}`
+                ) : (
+                  'All the posts'
+                )}
+              </h1>
+              {posts.data.length ? (
+                <Table columns={columns} dataSource={data} pagination={false} />
+              ) : (
+                <p>There is currently no posts in this category.</p>
+              )}
 
-        <div className="mt3 tc">
-          <Link className="blue pointer no-underline" to="/new">
-            <Button type="primary">add a new post</Button>
-          </Link>
+              <div className="mt3 tc">
+                <Link className="blue pointer no-underline" to="/new">
+                  <Button type="primary">add a new post</Button>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            <div className="pb3">
+              <h1>Oops... There is nothing here</h1>
+              <p>
+                {
+                  "The category you are trying to view don't exist in the database. Please return to the"
+                }{' '}
+                <Link to="/">Home Page</Link>
+              </p>
+            </div>
+          )}
         </div>
+      )
+    return (
+      <div className="flex justify-center">
+        <Spin size="large" />
       </div>
     )
   }
 }
 
 Posts.propTypes = {
-  category: PropTypes.string.isRequired,
+  category: PropTypes.string, // eslint-disable-line
+  categoryName: PropTypes.string.isRequired,
   dispatch: PropTypes.func.isRequired,
   posts: PropTypes.object.isRequired,
 }
 
-export default connect(({comments, posts}, ownProps) => ({
-  comments,
-  posts: {
-    data: Object.values(posts.data)
-      // remove deleted keep only selected category (if selected)
-      .filter(
-        ({category, deleted}) =>
-          !deleted && (!ownProps.category || category === ownProps.category),
-      )
-      // add commentsCount to the array
-      .map(post => ({
-        ...post,
-        commentsCount:
-          comments[post.id] &&
-          comments[post.id].data &&
-          comments[post.id].data.length,
-      }))
-      // order
-      .sort((post1, post2) => post2.voteScore > post1.voteScore),
-    status: posts.status,
-  },
-}))(Posts)
+export default connect(({categories, comments, posts}, ownProps) => {
+  const currentCategory = categories.data.find(
+    ({path}) => path === ownProps.category,
+  )
+  return {
+    comments,
+    category: ownProps.category,
+    categoryName: currentCategory ? currentCategory.name : '',
+    posts: {
+      data: Object.values(posts.data)
+        // remove deleted keep only selected category (if selected)
+        .filter(
+          ({category, deleted}) =>
+            !deleted && (!ownProps.category || category === ownProps.category),
+        )
+        // add commentsCount to the array
+        .map(post => ({
+          ...post,
+          commentsCount:
+            comments[post.id] &&
+            comments[post.id].data &&
+            comments[post.id].data.length,
+        }))
+        // order
+        .sort((post1, post2) => post2.voteScore > post1.voteScore),
+      status: posts.status,
+    },
+  }
+})(Posts)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,6 @@ const Home = props => {
   const {match: {params: {category}}} = props
   return (
     <div className="pa3 shadow-1 bg-near-white">
-      <h1 className="pl3 pb3 ma0 f3">
-        {category ? `Posts for category ${category}` : 'All the posts'}
-      </h1>
       <Posts category={category || ''} />
     </div>
   )
